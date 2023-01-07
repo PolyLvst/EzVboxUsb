f78c568794834c41462bf0acc21c50d4723543e9
PolyLvst <zeerofive05@gmail.com>
2023-01-07 21:31:56 +0700

    Run as admin check

--- EzVboxUsb.bat
+++ EzVboxUsb.bat
@@ -1,30 +1,41 @@
-@echo off
+@echo off & CLS & ECHO.
+NET FILE 1>NUL 2>NUL & IF ERRORLEVEL 1 (
+ECHO You must right-click and select
+ECHO "RUN AS ADMINISTRATOR"  to run this batch. Exiting... 
+ECHO.
+PAUSE 
+EXIT
+)
+REM ... proceed here with admin rights ...
 if not exist C:\Users\%username%\Documents\vmdk_files mkdir C:\Users\%username%\Documents\vmdk_files
 echo ---- Make a bootable usb for vbox ---
-echo Warning ! this script need admin privelages
-echo It's recommended to close VirtualBox first :D
+echo EzVboxUsb v0.2
 echo.
+
 :User_input
 set /p filename_vmdk=Type a name for it : 
 if exist C:\Users\%username%\Documents\vmdk_files\%filename_vmdk%.vmdk (
 echo file already existed 
 goto User_input
 )
+goto Disk_num
+
+:Disk_num
 start "" diskmgmt.msc
-set /p USBnum=Search for your usb disk number or removable disk : 
+set /p USBnum=Usb disk number or removable disk : 
 echo vmdk file can be found at C:\Users\%username%\Documents\vmdk_files
 goto Vbox_util
 
 :Vbox_util
 cd %programfiles%\Oracle\VirtualBox
+start "" VirtualBox.exe
 echo VBoxmanage createmedium disk --filename=C:\Users\%username%\Documents\vmdk_files\%filename_vmdk%.vmdk --variant=RawDisk --format=VMDK --property RawDrive=\\.\PhysicalDrive%USBnum%> "%cd%\temp_vbox_createrawvmdk.bat"
 call "%cd%\temp_vbox_createrawvmdk.bat"
 timeout /t 3
 del "%cd%\temp_vbox_createrawvmdk.bat"
-rem this command deprecated : VBoxManage internalcommands createmedium -filename C:users\%username%\documents\vmdk_files\%filename% -rawdisk \\.\PhysicalDrive%USBnum%
-echo Done my friend XD
-echo open folder?
-pause
-echo okay . . .
+echo done . . . 
 start C:\Users\%username%\Documents\vmdk_files
-pause
+goto eof
+rem this command deprecated : VBoxManage internalcommands createrawvmdk -filename C:users\%username%\documents\vmdk_files\%filename% -rawdisk \\.\PhysicalDrive%USBnum%
+
+:eof
